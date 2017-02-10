8079ff8ec05d5d7ab3669f9321b58831f180c128
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2017-02-10 11:21:57 +0000

    [OI] add support for plugins

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
+logstash_bin_dir = '/usr/share/logstash/bin'
 logstash_log_dir = '/var/log/logstash'
 logstash_conf_dir = '/etc/logstash/conf.d'
-logstash_home_dir = '/var/lib/logstash'
-logstash_install_dir = '/opt/logstash'
+logstash_home_dir = '/usr/share/logstash'
 
 describe group('logstash') do
   it { should exist }
@@ -22,18 +22,18 @@ end
   #{logstash_log_dir}
   #{logstash_home_dir}
   #{logstash_conf_dir}
-  #{logstash_install_dir}/bin
-  #{logstash_install_dir}/lib
+  #{logstash_home_dir}/bin
 ).each do |d|
   describe file(d) do
     it { should be_directory }
   end
 end
 
-%w(
-  /etc/init.d/logstash
+%W(
+  #{logstash_conf_dir}/../logstash.yml
+  #{logstash_conf_dir}/../jvm.options
+  #{logstash_conf_dir}/../log4j2.properties
   /etc/default/logstash
-  /etc/logrotate.d/logstash
 ).each do |f|
   describe file(f) do
     it { should be_file }
@@ -50,6 +50,11 @@ end
     it { should be_mode 644 }
     it { should be_owned_by 'logstash' }
   end
+end
+
+describe command("#{logstash_bin_dir}/logstash-plugin list logstash-filter-multiline") do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match 'logstash-filter-multiline' }
 end
 
 describe service('logstash') do
